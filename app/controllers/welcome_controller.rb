c6fd2906383fc4b4ea1f083f691ed58e7202d786
Bo Wen <bowenwen@users.noreply.github.com>
2021-10-16 22:50:21 -0700

    welcome page update and misc customizations

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
-    @google_maps_url = 'https://goo.gl/maps/nBAxNAsmPSS2'
-    @cool_earth_url = 'https://www.coolearth.org/'
+    @google_maps_venue_url = 'https://goo.gl/maps/dV5uUEsqHPq91xwV7'
+    @google_maps_parking_url = 'https://goo.gl/maps/bUZRKhAfxwz6S4ZG9'
   end
 end
