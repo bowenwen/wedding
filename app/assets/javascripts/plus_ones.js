86f150380b6e7d629b2639b8425ea2b939c224d1
Bo Wen <bowenwen@users.noreply.github.com>
2022-02-05 20:57:47 -0800

    improve kids meal selection ui behavior

--- app/assets/javascripts/plus_ones.js
+++ app/assets/javascripts/plus_ones.js
@@ -1,19 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+// logic for kids button
 $(function () {
   var checkMatcher = '#plus_ones-edit-form input[name="plus_one[child]"]'
   var checkBox = $(checkMatcher)
   function toggleKidsOnly () {
     var kids = checkBox.is(':checked')
-    // $('.guest-meal-option input').attr('disabled', kids == true)
-    // $('.guest-meal-option input').attr('checked', kids !== true) //don't use it is buggy
     if (kids) {
-      $('.guest-meal-option input[id="plus_one_meal_kids"]').attr('checked', true)
-      // $('.guest-meal-option input').attr('disabled', true)
+      $('.guest-meal-option input[id="plus_one_meal_kids"]').prop('hidden', false)
+      $('.guest-meal-option label[id="plus_one_meal_kids_label"]').prop('hidden', false)
+      $('.guest-meal-option input[id="plus_one_meal_kids"]').prop('checked', true)
       // console.log("Checkbox is checked..")
     } else {
-      $('.guest-meal-option input').attr('disabled', false)
+      $('.guest-meal-option input[id="plus_one_meal_chicken"]').prop('checked', true)
+      $('.guest-meal-option input[id="plus_one_meal_kids"]').prop('hidden', true)
+      $('.guest-meal-option label[id="plus_one_meal_kids_label"]').prop('hidden', true)
       // console.log("Checkbox is not checked..")
     }
   }
